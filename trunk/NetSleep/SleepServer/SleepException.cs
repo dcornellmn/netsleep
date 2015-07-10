ca1a75e7c0ddd90c136a501cb91fbca9df49fc0a
dcornell <dcornell@d2a6b1b3-a0d5-48f1-8376-b470e054eaee>
2015-07-10 19:14:51 +0000

    Added SleepServer class library - will be a component of SleepService. Borrowed the code framework for SleepServer from Wrox C# book.

---
+++ trunk/NetSleep/SleepServer/SleepException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCornell.NetSleep
+{
+    [Serializable]
+    public class SleepException : Exception
+    {
+        public SleepException() { }
+        public SleepException(string message) : base(message) { }
+        public SleepException(string message, Exception inner) : base(message, inner) { }
+        protected SleepException(
+        System.Runtime.Serialization.SerializationInfo info,
+        System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+}
---
+++ trunk/NetSleep/SleepServer/SleepServer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCornell.NetSleep
+{
+    public class SleepServer
+    {
+        private TcpListener listener;
+        private int port;
+        private Task listenerTask;
+
+        public SleepServer()
+            : this(9296)
+        {
+        }
+
+        public SleepServer(int port)
+        {
+            Contract.Requires<ArgumentException>(port >= IPEndPoint.MinPort && port <= IPEndPoint.MaxPort);
+
+            this.port = port;
+        }
+
+        protected bool GoToSleep()
+        {
+            return SleepInvoker.Suspend();
+        }
+
+        public void Start()
+        {
+            listenerTask = Task.Factory.StartNew(Listener, TaskCreationOptions.LongRunning);
+        }
+
+        protected void Listener()
+        {
+            try
+            {
+                IPAddress ipAddress = IPAddress.Any;
+                listener = new TcpListener(ipAddress, port);
+                listener.Start();
+                while (true)
+                {
+                    Socket clientSocket = listener.AcceptSocket();
+                    // TODO: Implement network communication
+                    clientSocket.Close();
+                }
+            }
+            catch (SocketException ex)
+            {
+                Trace.TraceError(String.Format("SleepServer {0}", ex.Message));
+                throw new SleepException("socket error", ex);
+            }
+        }
+
+        public void Stop()
+        {
+            listener.Stop();
+        }
+        public void Suspend()
+        {
+            listener.Stop();
+        }
+
+        private void StopListener()
+        {
+
+        }
+
+        public void Resume()
+        {
+            Start();
+        }
+
+
+    }
+}
