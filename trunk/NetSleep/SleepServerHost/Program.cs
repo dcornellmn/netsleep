1f63ae36616d819a89be73d359d983cc3d4962a5
dcornell <dcornell@d2a6b1b3-a0d5-48f1-8376-b470e054eaee>
2015-07-10 21:40:16 +0000

    Created SleepServerHost console application to provide a test harness for the SleepServer component.

---
+++ trunk/NetSleep/SleepServerHost/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCornell.NetSleep.SleepServerHost
+{
+    /// <summary>
+    /// Extremely simplistic console application that just runs a standalone SleepServer instance. Intended for testing/debugging purposes.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Default for testing
+            int port = 12345;
+
+            // Let the user pass in a port number
+            if (args.Length > 1)
+            {
+                try
+                {
+                    port = Int32.Parse(args[0]);
+                    if (port < 1 || port > 65534)
+                        throw new ArgumentOutOfRangeException("port", "TCP port number must be in the range of 1 to 65534");
+                }
+                catch
+                {
+                    Console.WriteLine("Usage: SleepServerHost [port]");
+                    Console.WriteLine("\nWhere port is the TCP port number to listen on (1-65534). Defaults to 12345.");
+                    return;
+                }
+            }
+
+            var srv = new SleepServer(port);
+            srv.Start();
+            Console.WriteLine("Server started on port %d. Press Enter to exit...", port);
+            Console.ReadLine();
+            srv.Stop();
+            Console.WriteLine("Server stopped.");
+        }
+    }
+}
