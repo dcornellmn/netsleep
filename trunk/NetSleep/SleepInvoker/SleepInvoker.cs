6aeedbd78a97bbe0e510d879406fdddf155fb567
dcornell <dcornell@d2a6b1b3-a0d5-48f1-8376-b470e054eaee>
2015-07-10 15:43:03 +0000

    Initial commit to SVN. SleepInvoker DLL and SleepCLI.exe are working. SleeperService has not yet been started.

---
+++ trunk/NetSleep/SleepInvoker/SleepInvoker.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows.Forms;
+
+namespace DCornell.NetSleep
+{
+    public class SleepInvoker
+    {
+        // Mark empty constructor as private - prevents instantiation
+        private SleepInvoker() { }
+
+        /// <summary>
+        /// Hibernates the system.
+        /// </summary>
+        /// <param name="force">True to force suspend to hibernation immediately; False (default) to send a suspend request to all running applications</param>
+        /// <returns>True if the system is being suspended to hibernate mode, or false if the suspend was canceled.</returns>
+        public static bool Hibernate(bool force = false)
+        {
+            return SuspendToState(PowerState.Hibernate, force);
+        }
+
+        /// <summary>
+        /// Suspends the system.
+        /// </summary>
+        /// <param name="force">True to force suspend immediately; False (default) to send a suspend request to all running applications</param>
+        /// <returns>True if the system is being suspended, or false if the suspend was canceled.</returns>
+        public static bool Suspend(bool force = false)
+        {
+            return SuspendToState(PowerState.Suspend, force);
+        }
+
+        /// <summary>
+        /// Suspend the system to the specified PowerState.
+        /// </summary>
+        /// <param name="state">PowerState indicating the suspend mode to use (currently Suspend or Hibernate)</param>
+        /// <param name="force">True to force suspend immediately; False (default) to send a suspend request to all running applications</param>
+        /// <returns>True if the system is being suspended to the specified mode, or false if the suspend was canceled.</returns>
+        public static bool SuspendToState(PowerState state, bool force)
+        {
+            return Application.SetSuspendState(state, force, false);
+        }
+    }
+}
