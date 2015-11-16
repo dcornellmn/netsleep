59f33785fd6869e26eb345ac836abb38db9b02b3
dcornell <dcornellmn@users.noreply.github.com>
2015-11-16 22:59:19 +0000

    Wrote some more server command-handling code.

--- NetSleep/SleepServer/SleepServer.cs
+++ NetSleep/SleepServer/SleepServer.cs
@@ -40,7 +40,7 @@ namespace DCornell.NetSleep
             {
                 listenerTask = Task.Factory.StartNew(Listener, TaskCreationOptions.LongRunning);
             }
-       }
+        }
 
         protected void Listener()
         {
@@ -52,16 +52,58 @@ namespace DCornell.NetSleep
                 while (true)
                 {
                     Socket clientSocket = listener.AcceptSocket();
-                    // TODO: Implement network communication
                     NetworkStream ns = new NetworkStream(clientSocket);
                     byte ch = 0;
+                    int i = 0;
                     byte[] cmd = new byte[256];
-                    for (int i = 0; ch != 13 && i < cmd.Length; i++)
+                    for (i = 0; ch != 13 && i < cmd.Length - 1; i++)
                     {
                         ns.Read(cmd, i, 1);
                     }
+                    cmd[i + 1] = 0; // null-terminate for safety
+
                     // process command
+                    char[] separators = { ' ', '\t', '\r', '\n' };
+                    string reply = "ERR SERVER\r\n"; // server's reply to client
+                    bool noclose = false; // keep connection open and process another command
+                    string[] cmdWords = Encoding.ASCII.GetString(cmd, 0, i).Split(separators, 3, StringSplitOptions.RemoveEmptyEntries);
+                    switch (cmdWords[0])
+                    {
+                        case "SLEEP":
+                        case "HIBER":
+                            // TODO: go to sleep
+                            reply = "OK\r\n";
+                            break;
+                        case "PWROFF":
+                        case "REBOOT":
+                            reply = "ERR NOTIMPL\r\n";
+                            break;
+                        case "VER":
+                            reply = "VER 1\r\n";
+                            noclose = true;
+                            break;
+                        default:
+                            reply = "ERR BADCMD\r\n";
+                            break;
+                    }
+#if DEBUG
+                    Console.WriteLine(string.Join(" ", "Debug: Received command:", cmdWords));
+#endif
+
+                    // send reply
+#if DEBUG
+                    Console.WriteLine("Debug: Sending reply: " + reply);
+#endif
+                    ns.Write(Encoding.ASCII.GetBytes(reply), 0, reply.Length);
+
+                    // close connection
+                    if (noclose) continue;
                     clientSocket.Close();
+
+                    // TODO: perform sleep action if command was valid
+#if DEBUG
+                    Console.WriteLine("Debug: Received valid command: " + cmdWords[0]);
+#endif
                 }
             }
             catch (SocketException ex)
