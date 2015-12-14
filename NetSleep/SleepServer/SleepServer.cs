b64cc8342a24e58b66de83e912319945be43adc2
dcornell <dcornellmn@users.noreply.github.com>
2015-12-14 20:45:33 +0000

    Fleshed out SleepServer code, including partial framework for some unimplemented future commands.

--- NetSleep/SleepServer/SleepServer.cs
+++ NetSleep/SleepServer/SleepServer.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.Contracts;
-using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -51,59 +48,95 @@ namespace DCornell.NetSleep
                 listener.Start();
                 while (true)
                 {
+                    // TODO: Fork the client handling code to a new thread and go back to listening
                     Socket clientSocket = listener.AcceptSocket();
                     NetworkStream ns = new NetworkStream(clientSocket);
-                    byte ch = 0;
-                    int i = 0;
-                    byte[] cmd = new byte[256];
-                    for (i = 0; ch != 13 && i < cmd.Length - 1; i++)
-                    {
-                        ns.Read(cmd, i, 1);
-                    }
-                    cmd[i + 1] = 0; // null-terminate for safety
 
-                    // process command
-                    char[] separators = { ' ', '\t', '\r', '\n' };
-                    string reply = "ERR SERVER\r\n"; // server's reply to client
-                    bool noclose = false; // keep connection open and process another command
-                    string[] cmdWords = Encoding.ASCII.GetString(cmd, 0, i).Split(separators, 3, StringSplitOptions.RemoveEmptyEntries);
-                    switch (cmdWords[0])
-                    {
-                        case "SLEEP":
-                        case "HIBER":
-                            // TODO: go to sleep
-                            reply = "OK\r\n";
-                            break;
-                        case "PWROFF":
-                        case "REBOOT":
-                            reply = "ERR NOTIMPL\r\n";
-                            break;
-                        case "VER":
-                            reply = "VER 1\r\n";
-                            noclose = true;
-                            break;
-                        default:
-                            reply = "ERR BADCMD\r\n";
-                            break;
-                    }
 #if DEBUG
-                    Console.WriteLine(string.Join(" ", "Debug: Received command:", cmdWords));
+                    IPEndPoint remote = (IPEndPoint) clientSocket.RemoteEndPoint;
+                    string remaddress = remote.Address.ToString();
+                    string remport = remote.Port.ToString();
+                    Console.WriteLine("Connection from " + remaddress + ":" + remport);
 #endif
 
-                    // send reply
+                    bool close = false; // conversation finished, close connection
+                    bool action_sleep = false; // go to sleep after conversation
+                    bool action_reboot = false; // reboot after conversation
+                    bool action_shutdown = false; // power down after conversation
+                    while (!close)
+                    {
+                        byte ch = 0;
+                        int i = 0;
+                        byte[] cmd = new byte[256];
+                        for (i = 0; ch != 13 && i < cmd.Length - 1; i++)
+                        {
+                            // TODO: Wrap this potentially blocking code with some sort of timer
+                            ns.Read(cmd, i, 1);
+                        }
+                        cmd[i + 1] = 0; // null-terminate for safety
+
+                        // process command
+                        char[] separators = { ' ', '\t', '\r', '\n' };
+                        string reply = "ERR SERVER\r\n"; // server's reply to client
+                        string[] cmdWords = Encoding.ASCII.GetString(cmd, 0, i).Split(separators, 3, StringSplitOptions.RemoveEmptyEntries);
+                        switch (cmdWords[0])
+                        {
+                            case "SLEEP":
+                            case "HIBER":
+                                action_sleep = true;
+                                reply = "OK\r\n";
+                                close = true;
+                                break;
+                            case "PWROFF":
+                            case "REBOOT":
+                                reply = "ERR NOTIMPL\r\n";
+                                close = true;
+                                break;
+                            case "VER":
+                                reply = "VER 1\r\n";
+                                close = false;
+                                break;
+                            case "CAPA":
+                            case "HELP":
+                                reply = "COMMANDS CAPA HIBER PWROFF QUIT REBOOT SLEEP VER\r\n\r\n";
+                                close = false;
+                                break;
+                            case "AUTH":
+                            case "HASH":
+                                reply = "ERR NOTIMPL\r\n";
+                                close = false;
+                                break;
+                            case "QUIT":
+                                reply = "OK\r\n";
+                                close = true;
+                                break;
+                            default:
+                                reply = "ERR BADCMD\r\n";
+                                close = true;
+                                break;
+                        }
 #if DEBUG
-                    Console.WriteLine("Debug: Sending reply: " + reply);
+                        Console.WriteLine(string.Join(" ", "Debug: Received command:", cmdWords));
 #endif
-                    ns.Write(Encoding.ASCII.GetBytes(reply), 0, reply.Length);
+
+                        // send reply
+#if DEBUG
+                        Console.WriteLine("Debug: Sending reply: " + reply);
+#endif
+                        ns.Write(Encoding.ASCII.GetBytes(reply), 0, reply.Length);
+                    }
 
                     // close connection
-                    if (noclose) continue;
+#if DEBUG
+                    Console.WriteLine("Debug: Closing client connection.");
+#endif
                     clientSocket.Close();
 
                     // TODO: perform sleep action if command was valid
-#if DEBUG
-                    Console.WriteLine("Debug: Received valid command: " + cmdWords[0]);
-#endif
+                    if (action_sleep)
+                    {
+                        Console.WriteLine("Simulated call to GoToSleep()");
+                    }
                 }
             }
             catch (SocketException ex)
